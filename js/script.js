5035d8ace0a8bcdc829ef937714a69c1f2b038bc
Pawel Godula <bolomaster7@gmail.com>
2022-09-05 20:02:02 +0200

    Logic changes

--- js/script.js
+++ js/script.js
@@ -8,12 +8,53 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
+function getMoveName(argMoveId){
+	if(argMoveId == 1){
+		
+	  return 'kamień';
+	} else if (argMoveId == 2){
+		return 'papier';
+	}
+	else if (argMoveId == 3){
+		return 'nożyce';
+	}
+	else
+	{
+	  printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+	  return 'nieznany ruch';
+	}
+  }
+function DisplayResult(argComputerMove, argPlayerMove){
+if ((argComputerMove == 'kamień' && argPlayerMove == 'papier') ||
+(argComputerMove == 'papier' && argPlayerMove == 'nożyce') ||
+(argComputerMove == 'nożyce' && argPlayerMove == 'kamień')){
+printMessage ('Wygrywasz');
+} else if (argComputerMove == argPlayerMove){
+printMessage ('Remis');
+} else{
+	printMessage ('Komputer wygrywa');
+	}
+	console.log (DisplayResult);
+}
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
+let argComputerMove = getMoveName(randomNumber);
 
+printMessage('Mój ruch to: ' + argComputerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let argPlayerMove = getMoveName(playerInput);
+printMessage('Twój ruch to: ' + argPlayerMove);
+
+DisplayResult();
+
+
+/*
 if(randomNumber == 1){
   computerMove = 'kamień';
 }
@@ -23,15 +64,11 @@ else if(randomNumber == 2){
   else if(randomNumber == 3){
 	computerMove = 'nożyce';
   }
+ */	
 
-printMessage('Mój ruch to: ' + computerMove);
 
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-console.log('Gracz wpisał: ' + playerInput);
-
-let playerMove = 'nieznany ruch';
-
+/*
 if(playerInput == '1'){
   playerMove = 'kamień';
 }
@@ -78,4 +115,4 @@ else if( computerMove == 'papier' && playerMove == 'papier'){
   }
   else if( computerMove == 'nożyce' && playerMove == 'nieznany ruch'){
 	printMessage('Remis!');
-  }						
+  }	 */					
